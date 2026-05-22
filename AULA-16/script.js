bade35b8093dca367a9749afc9d0b9cae16b6edb
Gabriel Marçal <marcalgabriel3@gmail.com>
2026-05-22 20:55:24 -0300

    Add files via upload

---
+++ AULA-16/script.js
@@ -0,0 +1,24 @@
+document.writeln('OLÁ MUNDO!!');
+
+// window.alert("Olá Mundo, seu lindo!!!");
+
+document.getElementById('Título').innerHTML = "Olá Mundo JavaScript";
+
+let nomePrompt = window.prompt("Olá Aluno de JavaScript, qual o seu nome?")
+
+document.getElementById('nomeAluno').innerHTML = ("O nome do aluno javascript é : " + nomePrompt)
+
+let resposta = window.confirm("Você gosta de JavaScript ?")
+
+if (resposta) {
+    document.getElementById('respostaaluno').innerHTML = "O aluno gosta de JavaScript"
+}
+else {
+    document.getElementById ('respostaaluno').innerHTML = "O aluno não gosta de JavaScript"
+}
+
+function exibeNomeAluno() {
+    let inputAluno = document.getElementById("inputNomeAluno").value;
+
+    document.getElementById("resposta").innerHTML = "O nome digitado foi: " + inputAluno;
+}
